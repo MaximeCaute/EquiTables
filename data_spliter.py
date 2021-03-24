0d9aeed129de752b28a8ceae6d25507e78c4d7c6
Maxime Cauté <maxime.caute@ens-rennes.fr>
2021-03-24 11:36:37 +0100

    Code factorisation

--- data_spliter.py
+++ data_spliter.py
@@ -4,30 +4,21 @@ Created: 17.03.2021
 
 This file is dedicated to the seperation of data into appropriate groups
 """
-def compute_subgroups_indices( data_to_match, data_to_differentiate,
-                            groups_size = 2):
-    """
-    This function computes indices for groups based on data.
 
-    WIP
-    ATM only computes indices over the last differentiation parameter
+def compute_groups_indices(data_to_differentiate):
+    """
+    This functions computes groups indices from boolean data to be differentiated.
+    Groups are created only if they have representants.
     --
     Input:
-        - data_to_match: pandas DataFrame.
-            The part of data that should be matched.
         - data_to_differentiate: pandas DataFrame.
             The part of data that should be differentiated.
-    Parameters:
-        - groups_size: int. The sizes of the group to create.
-            Defaults to groups of size 2 (TO BE CHANGED)
-    Outputs:
-        - subgroups_indices : int list list. The indices of the elements in each groups.
+            The values should be boolean.
+    Output:
+        - groups_indices: int set list.
+            The indices of the elements in each group.
             The i-th element contains the indices of the elements in group i.
     """
-    ## NOTE : FONCTION RENVOYER PDSERIES avec var indicatrices
-    # Ignorer les lignes non dans sous-groupes
-    #print(data_to_match)
-
     groups_indices = [set(data_to_differentiate.index)]
     for differentiation_parameter in data_to_differentiate:
         parameter_dataframe = data_to_differentiate[differentiation_parameter]
@@ -44,7 +35,35 @@ def compute_subgroups_indices( data_to_match, data_to_differentiate,
                     subgroups_indices.append(subgroup_indices)
 
         groups_indices = subgroups_indices
+    return groups_indices
 
+def compute_subgroups_indices( data_to_match, data_to_differentiate,
+                            groups_size = 2):
+    """
+    This function computes indices for groups based on data.
+
+    WIP
+    Note : does not check that differentiation columns have valid boolean values.
+    --
+    Input:
+        - data_to_match: pandas DataFrame.
+            The part of data that should be matched.
+        - data_to_differentiate: pandas DataFrame.
+            The part of data that should be differentiated.
+            The values should be boolean.
+    Parameters:
+        - groups_size: int. The sizes of the group to create.
+            Defaults to groups of size 2 (TO BE CHANGED)
+    Outputs:
+        - subgroups_indices : int list list.
+            The indices of the elements in each subgroup.
+            The i-th element contains the indices of the selected elements in subgroup i.
+    """
+    ## NOTE : FONCTION RENVOYER PDSERIES avec var indicatrices
+    # Ignorer les lignes non dans sous-groupes
+    #print(data_to_match)
+
+    groups_indices = compute_groups_indices(data_to_differentiate)
 
     subgroups_indices = [list(group_indices)[:groups_size] for group_indices
                                                     in groups_indices]
