e294bc14c095ddda33d3584757c1c47aac8b7cdf
Maxime Cauté <maxime.caute@ens-rennes.fr>
2021-03-29 20:22:37 +0200

    Added tree search with local heuristic - no backtracking

--- data_spliter.py
+++ data_spliter.py
@@ -4,6 +4,7 @@ Created: 17.03.2021
 
 This file is dedicated to the seperation of data into appropriate groups
 """
+from search_tree import SearchTree
 
 def compute_groups_indices(data_to_differentiate):
     """
@@ -33,17 +34,17 @@ def compute_groups_indices(data_to_differentiate):
 
                 if subgroup_indices != set():
                     subgroups_indices.append(subgroup_indices)
-
         groups_indices = subgroups_indices
     return groups_indices
 
-def compute_subgroups_indices( data_to_match, data_to_differentiate,
+def compute_subgroups_indices(data_to_match, data_to_differentiate,
                             groups_size = 2):
     """
     This function computes indices for groups based on data.
 
     WIP
     Note : does not check that differentiation columns have valid boolean values.
+    Note: currently works for only 2 groups
     --
     Input:
         - data_to_match: pandas DataFrame.
@@ -59,13 +60,34 @@ def compute_subgroups_indices( data_to_match, data_to_differentiate,
             The indices of the elements in each subgroup.
             The i-th element contains the indices of the selected elements in subgroup i.
     """
-    ## NOTE : FONCTION RENVOYER PDSERIES avec var indicatrices
-    # Ignorer les lignes non dans sous-groupes
-    #print(data_to_match)
+    NUM_GROUPS = 2
+    HEURISTIC = lambda s: next(iter(s)) #gets an element of the set #perhaps not clean to use sets?
+
+    ## NOTE : RETURN PDSERIES?
+    #Ignore lines not in subgroup
 
     groups_indices = compute_groups_indices(data_to_differentiate)
+    num_subgroups = len(groups_indices)
 
-    subgroups_indices = [list(group_indices)[:groups_size] for group_indices
-                                                    in groups_indices]
+    ## HARD SELECTION FOR 2-Group match -> TODO
+    #groups_indices = groups_indices[0:2]
 
-    return subgroups_indices
+    search_tree = SearchTree(groups_indices, groups_size)
+    for tuple_index in range(groups_size):
+        for subgroup_index in range(num_subgroups):
+            chosen_element_index = search_tree.current_node.compute_index_in_subgroup_in_tuple_with_local_heuristic(
+                    HEURISTIC,
+                    subgroup_index,
+                    tuple_index
+            )
+            search_tree.decide_index_for_subgroup_in_tuple_from_current_node(
+                chosen_element_index, subgroup_index, tuple_index
+            )
+    subgroups_indices_tuples = search_tree.get_current_solution()
+
+    subgroups_elements_indices = [[] for i in range(num_subgroups)]
+    for subgroups_indices_tuple in subgroups_indices_tuples:
+        for subgroup_index, subgroup_element_index in enumerate(subgroups_indices_tuple):
+            subgroups_elements_indices[subgroup_index].append(subgroup_element_index)
+
+    return subgroups_elements_indices
