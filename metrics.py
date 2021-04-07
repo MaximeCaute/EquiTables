98b3d637f9fa8b52d058e554b7ccd2bdd9ea132a
Maxime Cauté <maxime.caute@ens-rennes.fr>
2021-04-07 13:27:30 +0200

    fixed issues

--- metrics.py
+++ metrics.py
@@ -1,4 +1,4 @@
-br"""
+"""
 Author: Maxime Cauté
 Created: 07.04.2021
 
@@ -20,8 +20,8 @@ def compute_distance_from_tuples_and_group_indices(
                 normalized_data_to_match,
                 distance_metric = EUCLIDIAN_DISTANCE
     ):
-    baseline_element_index = baseline_tuple[subgroup1_index]
-    target_element_index = target_tuple[subgroup2_index]
+    baseline_element_index = baseline_tuple[baseline_group_index]
+    target_element_index = target_tuple[target_group_index]
 
     baseline_element_values = normalized_data_to_match.iloc[baseline_element_index].values
     target_element_values = normalized_data_to_match.iloc[target_element_index].values
@@ -35,7 +35,7 @@ def add_modified_distance_from_tuples_and_group_indices(
                 target_tuple,
                 baseline_group_index,target_group_index,
                 normalized_data_to_match,
-                distance_metric = EUCLIDIAN_DISTANCE
+                distance_metric = EUCLIDIAN_DISTANCE,
                 distance_modifier = lambda x : x**2
     ):
     elements_distance = compute_distance_from_tuples_and_group_indices(
@@ -52,13 +52,13 @@ def compute_distance_between_subgroups( element_indices_per_subgroup_per_tuple,
                                         distance_metric = EUCLIDIAN_DISTANCE
     ):
     subgroups_size = len(element_indices_per_subgroup_per_tuple)
-    num_subgroups = len(element_indices_per_subgroup_per_tuple[0])
+    num_groups = len(element_indices_per_subgroup_per_tuple[0])
     num_parameters = normalized_data_to_match.shape[1]
 
     distance = 0
 
-    subgroups_indices = range(num_subgroups)
-    for baseline_subgroup_index, target_subgroup_index in itertools.combinations(subgroups_indices, 2):
+    groups_indices = range(num_groups)
+    for baseline_group_index, target_group_index in itertools.combinations(groups_indices, 2):
         for baseline_tuple, target_tuple in itertools.combinations_with_replacement(element_indices_per_subgroup_per_tuple,2):
             distance = add_modified_distance_from_tuples_and_group_indices(
                 distance,
